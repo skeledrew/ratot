fd13492ebb3bbe41c5eec25adf765d4f0353d32b
Andrew Phillips <skeledrew@gmail.com>
2018-01-23 00:49:40 -0600

    Update router bot to use new anybot interface

--- ratot/router.py
+++ ratot/router.py
@@ -6,19 +6,16 @@ import os
 import rpyc
 
 sys.path.append(os.path.dirname(__file__))  # put in __init__?
-from anybot import AnyBot
+from anybot import AnyBot, Callback
 
 
 class RouterBot(AnyBot):
 
     def __init__(self, token):
         super().__init__(token)
-        setattr(self.bot, 'wrapper', self)
-        #pdb.set_trace()
         self._host = 'localhost'
         self._port = 18881
         self.commands = ['new']
-        self.load_handlers(rb_handlers())
         self._sessions = {}
         return
 
@@ -42,53 +39,37 @@ class RouterBot(AnyBot):
         self._sessions[name] = c
         return
 
+    def handle_text(self, bot, update):
+        in_ = update.message.text
+        #pdb.set_trace()
+        user = '{} {} ({})'.format(update.message.from_user.first_name, update.message.from_user.last_name, update.message.from_user.username)
+        self.write_log('{}: {} sent `{}`'.format(time_stamp(), user, in_))
+        if not update.message.from_user.username in ['skeledrew']:
+            msg = "You don't have permissions for this."
+            bot.send_message(chat_id=update.message.chat_id, text=msg)
+            return
+        c = rpyc.connect('localhost', 18881)
+        out_ = c.root.repl(in_)
 
-def rb_handlers():
-    # hack handlers into loader
-    return {'handle_start': handle_start, 'handle_text': handle_text, 'handle_ping': handle_ping, 'handle_new': handle_new}
+        bot.send_message(chat_id=update.message.chat_id, text=out_)
+        return
 
-def handle_text(bot=None, update=None, args=None):
-    if not update: return '.+'
-    in_ = update.message.text
-    #pdb.set_trace()
-    user = '{} {} ({})'.format(update.message.from_user.first_name, update.message.from_user.last_name, update.message.from_user.username)
-    bot.wrapper.write_log('{}: {} sent `{}`'.format(time_stamp(), user, in_))
-    if not update.message.from_user.username in ['skeledrew']:
-        msg = "You don't have permissions for this."
+    def start_cmd(self, bot, update):
+        user = update.message.from_user.first_name
+        msg = "Hi {}. This bot allows you to access a shell remotely, if you have permission.".format(user)
         bot.send_message(chat_id=update.message.chat_id, text=msg)
         return
-    c = rpyc.connect('localhost', 18881)
-    out_ = c.root.repl(in_)
 
-    bot.send_message(chat_id=update.message.chat_id, text=out_)
-    return
+    def ping_cmd(self, bot, update):
+        msg = 'pong!'
+        bot.send_message(chat_id=update.message.chat_id, text=msg)
+        return
 
-def handle_start(bot=None, update=None, args=None):
-    if not update: return '/start'
-    user = update.message.from_user.first_name
-    msg = "Hi {}. This bot allows you to access a shell remotely, if you have permission.".format(user)
-    bot.send_message(chat_id=update.message.chat_id, text=msg)
-    return
-
-def handle_ping(bot=None, update=None, args=None):
-    if not update: return '/ping'
-    msg = 'pong!'
-    bot.send_message(chat_id=update.message.chat_id, text=msg)
-    return
-
-def handle_command(bot=None, update=None, args=None):
-    # catch all
-    if not update: return ['new']
-    bot.wrapper.process_command(update)
-    return
-
-def handle_new(bot=None, update=None, args=None):
-    if not update: return '/new'
-    if bot == 'help': return 'Create a new session: `/new [session_name] [shell_type]`'
-    bot.wrapper.process_command(update)
-    return
+    def new_cmd(self, bot, update):
+        return
 def time_stamp():
     return time.asctime()
+
 
 
 if __name__ == '__main__':
