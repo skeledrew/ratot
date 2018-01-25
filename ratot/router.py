82369d8fe703d1447ce2a3d8e2ccfe6514d3c5ad
Andrew Phillips <skeledrew@gmail.com>
2018-01-24 21:13:21 -0600

    Change router and server to handle requests asyncronously

--- ratot/router.py
+++ ratot/router.py
@@ -1,24 +1,29 @@
 #! /usr/bin/env python3
 
 import pdb
+
 import sys
 import time
 import os
+from threading import Timer
 
 import rpyc
+import yaml
 
-sys.path.append(os.path.dirname(__file__))  # put in __init__?
+sys.path.append(os.path.dirname(__file__))  # put in __init__.py?
 from anybot import AnyBot, Callback
 
 
 class RouterBot(AnyBot):
 
-    def __init__(self, token):
-        super().__init__(token)
+    def __init__(self, token, config={}):
+        super().__init__(token, config)
         self._host = 'localhost'
         self._port = 18881
         self.commands = ['new']
         self._sessions = {}
+        self._async_ops = []
+        self._clean_interval = 5
         return
 
     def process_command(self, update):
@@ -43,18 +48,33 @@ class RouterBot(AnyBot):
 
     def handle_text(self, bot, update):
         in_ = update.message.text
-        #pdb.set_trace()
-        user = '{} {} ({})'.format(update.message.from_user.first_name, update.message.from_user.last_name, update.message.from_user.username)
-        self.write_log('{}: {} sent `{}`'.format(time_stamp(), user, in_))
-        if not update.message.from_user.username in ['skeledrew']:
+        uname = update.message.from_user.username
+        user = '{} {} ({})'.format(update.message.from_user.first_name, update.message.from_user.last_name or '_', uname)
+        self.log.info('{} sent `{}`'.format(user, in_))
+        if not uname in ['skeledrew']:
             msg = "You don't have permissions for this."
             bot.send_message(chat_id=update.message.chat_id, text=msg)
             return
-        c = rpyc.connect('localhost', 18881)
-        out_ = c.root.repl(in_)
-
-        bot.send_message(chat_id=update.message.chat_id, text=out_)
+        self.call_repl(uname, in_, bot.send_message, [], {'chat_id': update.message.chat_id})
         return
+
+    def call_repl(self, user, in_, cb=None, cb_args=[], cb_kwargs={}):
+        if not user in self._sessions: self._sessions[user] = {}
+        uss = self._sessions[user]
+
+        try:
+            if not 'default' in uss: uss['default'] = uss['__current__'] = rpyc.connect(self._host, self._port)
+            c = uss['__current__']
+            a_repl = rpyc.async(c.root.a_repl)
+            a_op = a_repl(in_, cb, cb_args, cb_kwargs, self.log)
+            self._async_ops.append(a_op)
+            Timer(1, self._force_wait, [a_op]).start()  # hacky; raise issue
+            self._clean_timer()
+
+        except Exception as e:
+            msg = 'Something broke en route: {}'.format(repr(e))
+            self.log.exception(msg)
+            return msg
 
     def start_cmd(self, bot, update):
         user = update.message.from_user.first_name
@@ -69,6 +89,20 @@ class RouterBot(AnyBot):
 
     def new_cmd(self, bot, update):
         return
+
+    def _clean_timer(self):
+        if not self._async_ops: return
+        self.log.info('Cleaning ready async ops')
+
+        for ao in self._async_ops:
+            if ao.ready: self._async_ops.remove(ao)
+        Timer(self._clean_interval, self._clean_timer).start()
+        return
+
+    def _force_wait(self, ao):
+        # attempt to fix that bug where async op only completes if value is called
+        res = ao.value
+        return res
 
 def time_stamp():
     return time.asctime()
--- ratot/server.py
+++ ratot/server.py
@@ -2,10 +2,13 @@
 
 import pdb
 import re
+import logging
+import sys
 
 import pexpect  # NB: patch pexpect/__init__.py with `import replwrap`
 import rpyc
 from rpyc import server  # NB: patch rpyc/__init__.py with `from rpyc.utils import server`
+logging.basicConfig(format='%(asctime)s (%(levelname)s): %(message)s', filename='{}.log'.format(sys.argv[0]), level=logging.DEBUG)
 
 
 class ShellSessionsService(rpyc.Service):
@@ -25,6 +28,23 @@ class ShellSessionsService(rpyc.Service):
         except Exception as e:
             print('Something broke: {}'.format(repr(e)))
             return e
+
+    def exposed_a_repl(self, in_, cb=None, args=[], kwargs={}, log=None):
+        out_ = None
+        logging.debug('Inside async repl')
+
+        try:
+            out_ = clean_ansi(self._bash.run_command(in_))
+            logging.debug('Command complete!')
+
+        except Exception as e:
+            out_ = 'Something broke. Check the logs if you have permission, or report to the admin.'
+            err = 'Something broke on the server: {}'.format(repr(e))
+            if err and logging: logging.exception(err)
+        kwargs['text'] = out_
+        logging.debug('Running callback {} in async with results:\n{}'.format(str(cb), out_))
+        cb(chat_id=kwargs['chat_id'], text=out_)
+        return out_
 
 
 def clean_ansi(text):
