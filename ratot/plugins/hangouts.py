09b3b31676ab138d4e8aa7476913ca20608a8c9f
Andrew Phillips <skeledrew@gmail.com>
2018-01-29 00:00:33 -0600

    Only initialize client if it doesn't exist

--- ratot/plugins/hangouts.py
+++ ratot/plugins/hangouts.py
@@ -122,19 +122,22 @@ class HC():
 
     def __init__(self):
         self._futures = []
+        self._client = None
 
     async def _async_main(self, coro, client, args):
         """Run the coroutine."""
         # Spawn a task for hangups to run in parallel with the example coroutine.
-        task = asyncio.ensure_future(client.connect())
+        if not self._client:
+            task = asyncio.ensure_future(client.connect())
 
-        # Wait for hangups to either finish connecting or raise an exception.
-        on_connect = asyncio.Future()
-        client.on_connect.add_observer(lambda: on_connect.set_result(None))
-        done, _ = await asyncio.wait(
-            (on_connect, task), return_when=asyncio.FIRST_COMPLETED
-        )
-        await asyncio.gather(*done)
+            # Wait for hangups to either finish connecting or raise an exception.
+            on_connect = asyncio.Future()
+            client.on_connect.add_observer(lambda: on_connect.set_result(None))
+            done, _ = await asyncio.wait(
+                (on_connect, task), return_when=asyncio.FIRST_COMPLETED
+            )
+            await asyncio.gather(*done)
+            self._client = client
 
         # Run the example coroutine. Afterwards, disconnect hangups gracefully and
         # yield the hangups task to handle any exceptions.
