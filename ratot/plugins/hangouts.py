a1e9411d46f4ab11b13dabb97d4ace74bd88da6b
Andrew Phillips <skeledrew@gmail.com>
2018-01-28 18:57:25 -0600

    Create Hangouts client by working backward from example

--- ratot/plugins/hangouts.py
+++ ratot/plugins/hangouts.py
@@ -4,6 +4,9 @@ import pdb
 
 import sys
 import os
+import asyncio
+from threading import Timer
+import logging
 
 import hangups
 from hangups.ui.utils import get_conv_name
@@ -18,7 +21,6 @@ class HangoutsClient():
         self._conv_list = None
         self._user_list = None
         refresh_token_path = config.get('refresh_token_path', None) or os.path.join(os.path.dirname(__file__), 'hangouts.token')
-        #if not refresh_token_path: refresh_token_path =
 
         try:
             cookies = hangups.auth.get_auth_stdin(refresh_token_path)
@@ -27,6 +29,10 @@ class HangoutsClient():
             sys.exit('Login failed ({})'.format(repr(e)))
         self._client = hangups.Client(cookies)
         self._client.on_connect.add_observer(self._on_connect)
+        self._connected = False
+        logging.basicConfig(level=logging.DEBUG)
+        self.log = logging
+        self._event_loop = None
 
     async def _connect(self):
         await self._client.connect()
@@ -39,6 +45,7 @@ class HangoutsClient():
         )
 
         self._conv_list.on_event.add_observer(self._on_event)
+        return
 
     def _on_event(self, conv_event):
         conv = self._conv_list.get(conv_event.conversation_id)
@@ -48,9 +55,145 @@ class HangoutsClient():
             not user.is_self,
             not conv.is_quiet,
         ))
+        return
+
+    def _run_event_loop(self, task):
+        loop = asyncio.get_event_loop()
+        self._event_loop = loop
+        #if task:
+        #loop.close()
+        return
+
+    def run_event_loop(self, task=None):
+        # run loop in a different thread
+        self._loop_wrap_timer = Timer(0, self._run_event_loop, [task]).start()
+        return
+
+    async def _disconnect(self):
+        await self._client.disconnect()
+        return
+
+    async def _list_coro(self, client):
+        user_list, conversation_list = (
+        await hangups.build_user_conversation_list(client)
+)
+        all_users = user_list.get_all()
+        all_conversations = conversation_list.get_all(include_archived=True)
+
+        print('{} known users'.format(len(all_users)))
+        for user in all_users:
+            print('    {}: {}'.format(user.full_name, user.id_.gaia_id))
+        print('{} known conversations'.format(len(all_conversations)))
+        return
+
+    def list_cmd(self):
+        self.run_it(self._list_coro)
+        return
+
+    def run_it(self, coro):
+         task = asyncio.ensure_future(self._async_main(coro, self._client),
+loop=self._event_loop)
+         if not self._connected:
+             self.log.warning('Client not yet connected to server.')
+             return
+         self._event_loop.run_until_complete(task)
+
+    async def _async_main(self, coro, client):
+
+        if not self._connected:
+            task = asyncio.ensure_future(client.connect())
+            on_connect = asyncio.Future()
+            client.on_connect.add_observer(lambda: on_connect.set_result(None))
+            done, _ = await asyncio.wait(
+                (on_connect, task),
+                return_when=asyncio.FIRST_COMPLETED
+            )
+            await asyncio.gather(*done)
+
+        try:
+            await coro(client)
+
+        except Exception as e:
+            self.log.exception('Something broke: {}'.format(repr(e)))
+        return
+
+
+class HC():
+
+    @staticmethod
+    async def _async_main(example_coroutine, client, args):
+        """Run the example coroutine."""
+        # Spawn a task for hangups to run in parallel with the example coroutine.
+        task = asyncio.ensure_future(client.connect())
+
+        # Wait for hangups to either finish connecting or raise an exception.
+        on_connect = asyncio.Future()
+        client.on_connect.add_observer(lambda: on_connect.set_result(None))
+        done, _ = await asyncio.wait(
+            (on_connect, task), return_when=asyncio.FIRST_COMPLETED
+        )
+        await asyncio.gather(*done)
+
+        # Run the example coroutine. Afterwards, disconnect hangups gracefully and
+        # yield the hangups task to handle any exceptions.
+        try:
+            await example_coroutine(client, args)
+        except asyncio.CancelledError:
+            pass
+        finally:
+            await client.disconnect()
+            await task
+
+    @staticmethod
+    def run_it(example_coroutine, *extra_args):
+        """Run a hangups example coroutine.
+        Args:
+            example_coroutine (coroutine): Coroutine to run with a connected
+                hangups client and arguments namespace as arguments.
+            extra_args (str): Any extra command line arguments required by the
+                example.
+        """
+        args = []
+        logging.basicConfig(level=logging.WARNING)
+        # Obtain hangups authentication cookies, prompting for credentials from
+        # standard input if necessary.
+        cookies = hangups.auth.get_auth_stdin('hangouts.token')
+        client = hangups.Client(cookies)
+        loop = asyncio.get_event_loop()
+        task = asyncio.ensure_future(HC._async_main(example_coroutine, client, args),
+                                     loop=loop)
+
+        try:
+            loop.run_until_complete(task)
+        except KeyboardInterrupt:
+            task.cancel()
+            loop.run_until_complete(task)
+        finally:
+            loop.close()
+
+    @staticmethod
+    async def sync_recent_conversations(client, _):
+        user_list, conversation_list = (
+            await hangups.build_user_conversation_list(client)
+        )
+        all_users = user_list.get_all()
+        all_conversations = conversation_list.get_all(include_archived=True)
+
+        print('{} known users'.format(len(all_users)))
+        for user in all_users:
+            print('    {}: {}'.format(user.full_name, user.id_.gaia_id))
+
+        print('{} known conversations'.format(len(all_conversations)))
+        for conversation in all_conversations:
+            if conversation.name:
+                name = conversation.name
+            else:
+                name = 'Unnamed conversation ({})'.format(conversation.id_)
+                print('    {}'.format(name))
 
 
 if __name__ == '__main__':
     # setup a REPL
-    hc = HangoutsClient()
+    #hc = HangoutsClient()
+    HC.run_it(HC.sync_recent_conversations)
     pdb.set_trace()
