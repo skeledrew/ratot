849193ca82a15a79f8c23045f3e38001bf3b19ce
Andrew Phillips <skeledrew@gmail.com>
2018-01-28 20:18:16 -0600

    Move event loop to another thread

--- ratot/plugins/hangouts.py
+++ ratot/plugins/hangouts.py
@@ -120,9 +120,11 @@ loop=self._event_loop)
 
 class HC():
 
-    @staticmethod
-    async def _async_main(example_coroutine, client, args):
-        """Run the example coroutine."""
+    def __init__(self):
+        self._futures = []
+
+    async def _async_main(self, coro, client, args):
+        """Run the coroutine."""
         # Spawn a task for hangups to run in parallel with the example coroutine.
         task = asyncio.ensure_future(client.connect())
 
@@ -137,18 +139,18 @@ class HC():
         # Run the example coroutine. Afterwards, disconnect hangups gracefully and
         # yield the hangups task to handle any exceptions.
         try:
-            await example_coroutine(client, args)
+            await coro(client, args)
         except asyncio.CancelledError:
             pass
         finally:
             await client.disconnect()
             await task
+        return
 
-    @staticmethod
-    def run_it(example_coroutine, *extra_args):
-        """Run a hangups example coroutine.
+    def run_coro(self, coro, *extra_args):
+        """Run a hangups coroutine.
         Args:
-            example_coroutine (coroutine): Coroutine to run with a connected
+            coro (coroutine): Coroutine to run with a connected
                 hangups client and arguments namespace as arguments.
             extra_args (str): Any extra command line arguments required by the
                 example.
@@ -159,20 +161,28 @@ class HC():
         # standard input if necessary.
         cookies = hangups.auth.get_auth_stdin('hangouts.token')
         client = hangups.Client(cookies)
-        loop = asyncio.get_event_loop()
-        task = asyncio.ensure_future(HC._async_main(example_coroutine, client, args),
-                                     loop=loop)
+        self._event_loop = asyncio.get_event_loop()
+        self.run_event_loop()
+        coro = self._async_main(coro, client, args)
+        future = asyncio.run_coroutine_threadsafe(coro, self._event_loop)
+        self._futures.append(future)
 
+    def _run_event_loop(self, loop):
         try:
-            loop.run_until_complete(task)
+            loop.run_forever()
         except KeyboardInterrupt:
-            task.cancel()
-            loop.run_until_complete(task)
+            #task.cancel()
+            #loop.run_until_complete(task)
+            pass
         finally:
             loop.close()
+        return
 
-    @staticmethod
-    async def sync_recent_conversations(client, _):
+    def run_event_loop(self):
+        Timer(0, self._run_event_loop, [self._event_loop]).start()
+        return
+
+    async def sync_recent_convos(self, client, _):
         user_list, conversation_list = (
             await hangups.build_user_conversation_list(client)
         )
@@ -190,10 +200,17 @@ class HC():
             else:
                 name = 'Unnamed conversation ({})'.format(conversation.id_)
                 print('    {}'.format(name))
+        return
+
+    def demo(self):
+        self.run_coro(self.sync_recent_convos)
+        return
 
 
 if __name__ == '__main__':
     # setup a REPL
     #hc = HangoutsClient()
-    HC.run_it(HC.sync_recent_conversations)
+    #HC.run_it(HC.sync_recent_convos)
+    hc = HC()
+    hc.demo()
     pdb.set_trace()
